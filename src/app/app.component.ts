1d5edf4233ae36528d3eb7c6286af0cf6ab82bcf
Hvilsted <lahvi22@student.sdu.dk>
2024-05-06 01:53:20 +0200

    changes to projects & start on about

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,13 @@ import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 import { AboutComponent } from './about/about.component';
 import { ProjectsComponent } from './projects/projects.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, AboutComponent, ProjectsComponent],
+  imports: [RouterOutlet, HeaderComponent, NavbarComponent, AboutComponent, ProjectsComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
