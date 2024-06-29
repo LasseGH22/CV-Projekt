f3bf0eb8d3586b357a95cc8e9aef7c0e94393c8a
Hvilsted <lahvi22@student.sdu.dk>
2024-06-29 02:50:03 +0200

    About text done

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -15,22 +15,22 @@ export class AboutComponent {
     {
       index: 0,
       title: ["E A R L Y", "D A Y S"],
-      description: "I have always been very interested in tech, especially computers. Computer games has therefore been a big part of my life and is still a great hobby of mine. Through the years of gaming a curiosity for how my favorite games are made has grown."
+      description: "I have always had a strong interest in technology, especially computers. As a result, Computer games have played a significant part in my life and to this day remains a great hobby of mine. During my early years of gaming, I developed a curiosity for how my favorite games were made. Although I had absolutetly no understanding of how they were made, they fascinated me."
     },
     {
       index: 1,
       title: ["P R I M A R Y", "S C H O O L"],
-      description: "The curiosity kept growing and began to turn into a general interest for software and the different applications i used on a daily basis. It was in primary school i did my first programming as i joined an elective course teaching low level block-based software development. Besides the very low level of programming being taught, i also joined an extra course introducing me to the very basics of web development. I remember creating a very simple website, using only HTML and CSS, with the sole purpose of redirecting people to my Steam profile' trade section in hopes of getting free stuff."
+      description: "My curiosity continued to grow, evolving into a broader interest in software and the various applications I used daily. In primary school, I took my first steps into programming by joining an elective course that taught low-level block-based software development. Alongside this, I enrolled in an optional course that introduced me to the very basics of web development. I vividly remember creating a simple website using only HTML and CSS, designed solely to redirect people to my Steam profile's trade section in hopes of getting free items."
     },
     {
       index: 2,
       title: ["H I G H", "S C H O O L"],
-      description: "Once finished with Primary School choosing the specialization of my High School degree was at that point an easy task. I chose to study a Higher Technical Exam (HTX) with a specialization of Programming and Communication-and-It. I was taught the fundamentals of object oriented programming in C# and i also did some Arduino programming and the needed electronics work."
+      description: "After completing primary school, choosing a specialization and program for high school was an easy decision. I opted for a Higher Technical Examination (HTX) with a focus on Programming and Communication-and-IT. During this time, I learned the fundamentals of object-oriented programming in C# and gained experience in Arduino programming and the necessary electronics work. It was at this point, I started to enjoy the brain-twisting challenges and intricate thinking involved in software development."
     },
     {
       index: 3,
       title: ["U N I V E R S I T Y"],
-      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur dapibus ipsum velit, ut mollis felis venenatis vel. Curabitur erat lacus, tempus non malesuada vitae, fermentum sit amet eros. Praesent mollis quam et leo ultricies pulvinar. Pellentesque eleifend lectus at ex elementum ornare. Pellentesque molestie rutrum turpis sit amet viverra. Aenean faucibus ullamcorper ipsum, vulputate lobortis mauris mattis eu. Mauris nec magna quam."
+      description: "After high school, I decided to pursue a BEng in Software Technology at the University of Southern Denmark. The program has, so far, provided me with a comprehensive understanding of software development, covering software engineering principles, advanced object-oriented programming, and in-depth web development. I've also gained experience with databases, software architecture, design patterns, software testing, and more. I have applied this knowledge in various projects, both individually and in groups. The contrast between working individually and in groups has taught me the significance and advantages of a strong team dynamic and communication between all involved."
     }
   ]
 }
--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,47 +1,11 @@
 import { Component, ViewChild } from '@angular/core';
-import { CustomNotificationComponent } from '../custom-notification/custom-notification.component';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [CustomNotificationComponent],
+  imports: [],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
-  @ViewChild('customNotification') customNotification!: CustomNotificationComponent;
-
-  constructor() {
-    this.customNotification = new CustomNotificationComponent();
-  }
-
-  CopyToClipboard(): void {
-    const copyText = document.getElementById("Email");
-  
-    if (copyText) {
-      navigator.clipboard.writeText(copyText.textContent || '')
-        .then(() => {
-          this.customNotification.sucess = true;
-          this.customNotification.show("Email copied to clipboard");
-        })
-        .catch(err => {
-          this.customNotification.sucess = false;
-          console.error('Error in copying text: ', err);
-          this.customNotification.show("Failed copy to clipboard");
-        });
-    } else {
-      console.log("Element with ID 'Email' not found.");
-    }
-  }
-
-  onHover(): void {
-    const emailElement = document.getElementById('Email');
-    emailElement?.classList.add('text-blue-500', 'underline');
-  }
-  
-  offHover(): void {
-    const emailElement = document.getElementById('Email');
-    emailElement?.classList.remove('text-blue-500', 'underline');
-  }
-
 }
